683561b84c83fcc865a8433a5fcac873c196b9aa
Marcin Chojnacki <marcin.chojnacki@nokia.com>
2020-11-06 12:59:08 +0100

    Fixed logging to file

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	k8s.io/apimachinery v0.0.0-20190913080033-27d36303b655
 	//k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	k8s.io/klog v0.4.0
 	//k8s.io/klog v0.4.0
 	sigs.k8s.io/controller-runtime v0.4.0
 )
--- main.go
+++ main.go
@@ -28,13 +28,14 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/klog"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	// +kubebuilder:scaffold:imports
 )
 
 const (
-	defaultPort int = 9443
+	defaultWebhooksPort int = 9443
 )
 
 var (
@@ -50,6 +51,11 @@ func init() {
 }
 
 func main() {
+	// prevent logging into file by glog
+	flag.Set("logtostderr", "true")
+	// prevent logging into file by klog
+	klog.SetOutput(os.Stdout)
+
 	var metricsAddr string
 	var healthcheckAddr string
 	var webhooksPort string
@@ -57,18 +63,18 @@ func main() {
 	var clusterResourceNamespace string
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&healthcheckAddr, "healthcheck-addr", ":8081", "The address the healthcheck endpoints binds to.")
-	flag.StringVar(&webhooksPort, "webhooks-port", strconv.Itoa(defaultPort), "Port for webhooks requests.")
+	flag.StringVar(&webhooksPort, "webhooks-port", strconv.Itoa(defaultWebhooksPort), "Port for webhooks requests.")
 	port, err := strconv.Atoi(webhooksPort)
 	if err != nil {
 		setupLog.Error(err, "invalid webhooks port. Using default.")
-		port = defaultPort
+		port = defaultWebhooksPort
 	}
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&clusterResourceNamespace, "cluster-resource-namespace", "kube-system", "Namespace where cluster-level resources are stored.")
 	flag.Parse()
 
-	ctrl.SetLogger(zap.Logger(true))
+	ctrl.SetLogger(zap.Logger(false))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
